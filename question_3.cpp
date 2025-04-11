4d9b69fae196aaf4519862860b1486760a3e1bb8
Nurulshaikh786 <shaikhnurul8200@gmail.com>
2025-04-11 10:56:36 +0530

    Update question_3.cpp

--- question_3.cpp
+++ question_3.cpp
@@ -2,24 +2,17 @@
 
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int num, last_dig, first_dig, sum;
-
     cout << "Enter a number : ";
     cin >> num;
-
     last_dig = num % 10;
-
     while (num != 0)
     {
-
         first_dig = num % 10;
         num = num / 10;
     }
-
     sum = first_dig + last_dig;
-
     cout << "Sum of first and last digit of entered number is : " << sum;
 }
