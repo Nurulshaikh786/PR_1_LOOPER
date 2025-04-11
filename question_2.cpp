5363f6d25de676e51b5099d331c2ab1050167c84
Nurulshaikh786 <shaikhnurul8200@gmail.com>
2025-04-11 10:56:15 +0530

    Update question_2.cpp

--- question_2.cpp
+++ question_2.cpp
@@ -2,20 +2,16 @@
 
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int num, count = 0;
     cout << "Enter a number : ";
     cin >> num;
-
     while (num != 0)
     {
         num = num / 10;
         count++;
     }
-
     cout << "Enterd Number contain " << count << " digits";
-
     return 0;
 }
